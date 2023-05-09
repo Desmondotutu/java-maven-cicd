e95a1e3e954255585a3941b582ffcb14061e3e87
Desmondotutu <dwangki2@gmail.com>
2023-05-09 21:57:48 +0100

    all changes pushed to github repo

---
+++ src/main/java/com/desmondo/desmondo/DesmondoApplication.java
@@ -0,0 +1,13 @@
+package com.desmondo.desmondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesmondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesmondoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desmondo/desmondo/DesmondoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desmondo.desmondo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesmondoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
